8b4a8b69f136cacefb02611528f5718d63c97824
Kianoosh Sanatkar <48183332+kianooshsanatkar@users.noreply.github.com>
2019-11-24 02:55:43 +0330

    Test Author name

--- runtests.py
+++ runtests.py
@@ -7,9 +7,10 @@ test_modules_list = [
 ]
 
 if __name__ == '__main__':
+
     tests = unittest.TestSuite()
+
     for module in  test_modules_list:
         tests.addTest(unittest.TestLoader().discover(module, '*test.py'))
-
 
     unittest.TextTestRunner(verbosity=2).run(tests)
