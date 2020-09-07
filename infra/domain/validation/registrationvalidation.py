6c5ec73df4edb71829513df5308a24a729adb647
Kianoosh Sanatkar <48183332+kianooshsanatkar@users.noreply.github.com>
2020-09-08 00:51:45 +0430

    Initialize -> RegistrationInfo Validation Entity

---
+++ infra/domain/validation/registrationvalidation.py
@@ -0,0 +1,26 @@
+from core.auxiliary.helper import email_validator, extract_numbers, mobile_number_validator
+from core.domain.ConventionalObjects import Email, CellPhone
+from infra.domain.entities.user import RegistrationInfo
+from core.exceptionhandler.exceptions import ValidationException
+from infra.resource import ResourceManager, Texts
+
+
+def email_validation(email: Email) -> None:
+    email_str = str(email).lower()
+    if not email_validator(email_str):
+        raise ValidationException(ResourceManager.translate(Texts.EMAIL_NOT_VALID))
+
+
+def cellphone_validation(cellphone: CellPhone) -> None:
+    if not mobile_number_validator(str(cellphone)):
+        raise ValidationException(ResourceManager.translate(Texts.MOBILE_NOT_VALID))
+
+
+def registration_validation(user: RegistrationInfo):
+    if not user.email and not user.cellphone:
+        raise ValidationException(ResourceManager.translate(Texts.USER_NEED_CONTACT))
+    if user.email:
+        user.cellphone.read(extract_numbers(str(user.cellphone)))
+        email_validation(user.email)
+    if user.cellphone:
+        cellphone_validation(user.cellphone)
