4a3af5ce5dd2cf5d7c63fe66aa746e19d34716d7
Kianoosh Sanatkar <48183332+kianooshsanatkar@users.noreply.github.com>
2020-09-10 19:48:13 +0430

    separate AuthInfo Verification

--- infra/domain/entities/user.py
+++ infra/domain/entities/user.py
@@ -1,4 +1,4 @@
-from core.auxiliary.helper import *
+from core.auxiliary.helper import is_hashed, hash_match
 from core.domain.baseentity import Entity
 from core.exceptionhandler.exceptions import ValidationException, AuthenticationException
 from domain.ConventionalObjects import Email, CellPhone
@@ -8,50 +8,36 @@ from infra.domain.valueobject import UserState
 
 class User(Entity):
 
-    def __init__(self, uid=None, user_name=None, state=UserState.REGISTERED):
+    def __init__(self, uid=None, user_name=None, state=UserState.DEACTIVATE):
         super().__init__(uid)
         self.user_name = user_name
         self.state = state
 
 
 class AuthInfo(User):
-    __hide_password = False
 
-    def __init__(self, uid=None, user_name=None, state=UserState.REGISTERED, password=None):
+    def __init__(self, uid=None, user_name=None, state=UserState.DEACTIVATE, password=None):
         super().__init__(uid, user_name, state)
         if password is not None:
-            if is_hashed(password):
-                self.__password = password
-            else:
-                self.set_new_password(password)
+            self.password = password
 
     @property
     def password(self):
-        # return '' if self.__hide_password is True else self.__password
         return self.__password
 
-    def password_hidden(self, hide):
-        self.__hide_password = hide
+    @password.setter
+    def password(self, value):
+        if isinstance(value, str):
+            if is_hashed(value):
+                self.__password = value
+                return
 
-    def set_new_password(self, password: str):
-        self.password_validation(password)
-        self.__password = hashing_string(password)
-
-    def password_validation(self, password: str = None):
-        if password_validation(self.password if password is None else password) is not True:
-            raise ValidationException(ResourceManager.translate(Texts.PASSWORD_VALUE_NOT_VALID))
-
-    def password_verification(self, password: str) -> None:
-        if hash_match(self.__password, password) is not True:
-            raise AuthenticationException(ResourceManager.translate(Texts.LOGIN_VALUE_INCORRECT))
-
-    def validation(self):
-        self.password_validation(self.password)
+        raise ValueError("password must be a hashed")
 
 
 class RegistrationInfo(User):
 
-    def __init__(self, uid=None, user_name=None, state=UserState.REGISTERED, email: Email = None,
+    def __init__(self, uid=None, user_name=None, state=UserState.DEACTIVATE, email: Email = None,
                  cellphone: CellPhone = None, persons=None, current_person=None):
         super().__init__(uid, user_name, state)
         self.cellphone = cellphone
---
+++ infra/domain/services/authservice.py
@@ -0,0 +1,15 @@
+from auxiliary.helper import hash_match, hashing_string
+from infra.domain.validation.authvalidation import password_validation
+from exceptionhandler.exceptions import AuthenticationException
+from infra.domain.entities.user import AuthInfo
+from infra.resource import ResourceManager, Texts
+
+
+def password_verification(auth:AuthInfo, password: str) -> None:
+    if hash_match(auth.password, password) is not True:
+        raise AuthenticationException(ResourceManager.translate(Texts.LOGIN_VALUE_INCORRECT))
+
+
+def set_new_password(self, password: str):
+    password_validation(password)
+    self.__password = hashing_string(password)
