b90a2e6e07feeb1360691d9242a1083dc40a2114
Kianoosh Sanatkar <48183332+kianooshsanatkar@users.noreply.github.com>
2020-09-28 15:42:33 +0330

    Add sqlauowmanager.py: UnitOfWork Configuration Manager for SQLAlchemy Repository Implementation

---
+++ core/db/slqalchemyrepo/sqlauowmanager.py
@@ -0,0 +1,19 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker, Session
+
+from db.repo.iuowmanager import IUnitOfWorkConfiguration
+
+
+class UowConfiguration(IUnitOfWorkConfiguration):
+    def __init__(self, db, url=None, create=False):
+        self.engine = create_engine(db.connection_str, echo=db.echo) if url is None else \
+            create_engine(url, echo=db.echo)
+        if create is True:
+            db.Base.metadata.create_all(self.engine)
+        self.__Session__ = sessionmaker(bind=self.engine)
+
+    def get_context(self) -> Session:
+        return self.__Session__()
+
+    # def __exit__(self, exc_type, exc_val, exc_tb):
+    #     self.engine.dispose()
