b445780528ab4ef043b7a5114324b8c3b7adf4b0
Kianoosh Sanatkar <48183332+kianooshsanatkar@users.noreply.github.com>
2020-10-27 21:00:15 +0330

    Complete User Service Domain authservice.py: add create new user (create_user_auth) function

--- infra/domain/services/authservice.py
+++ infra/domain/services/authservice.py
@@ -1,15 +1,23 @@
-from auxiliary.helper import hash_match, hashing_string
+from core.exceptionhandler.exceptions import AuthenticationException
+from infra.auxiliary.helper import hash_match, hashing_string
+from infra.domain.entities.authentication import AuthInfo
+from infra.domain.entities.user import UserState
+from infra.domain.validation import auth_validation
 from infra.domain.validation.authvalidation import password_validation
-from exceptionhandler.exceptions import AuthenticationException
-from domain.entities.authentication import AuthInfo
 from infra.resource import ResourceManager, Texts
 
 
-def password_verification(auth:AuthInfo, password: str) -> None:
+def password_verification(auth: AuthInfo, password: str) -> None:
     if hash_match(auth.password, password) is not True:
         raise AuthenticationException(ResourceManager.translate(Texts.LOGIN_VALUE_INCORRECT))
 
 
-def set_new_password(self, password: str):
+def new_password(password: str):
     password_validation(password)
-    self.__password = hashing_string(password)
+    return hashing_string(password)
+
+
+def create_user_auth(username: str, state: UserState, password: str) -> AuthInfo:
+    auth = AuthInfo(None, username.lower(), state, new_password(password))
+    auth_validation(auth)
+    return auth
