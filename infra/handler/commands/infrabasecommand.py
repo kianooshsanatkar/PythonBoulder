f09cc84412d48360ac5ca29ff1509cfcbb1dd957
Kianoosh Sanatkar <48183332+kianooshsanatkar@users.noreply.github.com>
2020-10-28 20:26:17 +0330

    redesign infrabasecommand.py insert uow and adapter though injection

--- infra/handler/commands/infrabasecommand.py
+++ infra/handler/commands/infrabasecommand.py
@@ -1,12 +1,10 @@
-from abc import ABC, ABCMeta
+from abc import ABC
 
 from core.handler.basecq import BaseCommand
-from infra.datahandler.repository import InfraRepository
-from infra.translator.toentity import ObjectModelTranslator
-from infra.translator.toobjectmodel import EntityTranslator
 
 
 class InfraBaseCommand(BaseCommand, ABC):
-    __data_handler__: InfraRepository
-    __entity_translator__ = EntityTranslator
-    __model_translator__ = ObjectModelTranslator
+
+    def __init__(self, user, uow, adapter):
+        super().__init__(user, uow)
+        self.adp = adapter
