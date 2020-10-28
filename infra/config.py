928f3790cf9e361e277326b199279447dbca67bb
Kianoosh Sanatkar <48183332+kianooshsanatkar@users.noreply.github.com>
2020-10-28 20:30:50 +0330

    add configuration in infra

---
+++ infra/config.py
@@ -0,0 +1,12 @@
+MongoDb = {
+    'db': 'EF_INFRA',
+    'alias': 'INFRA',
+    'host': None,
+    'port': None
+}
+MongoTestDb = {
+    'db': 'EF_User_Test',
+    'alias': 'INFRA',
+    'host': None,
+    'port': None
+}
